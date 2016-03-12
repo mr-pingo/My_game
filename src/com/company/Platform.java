7cb03e0eb13d8fd48e13401af97f85e7eaa49231
mr-pingo <iliya936@yandex.ru>
2016-03-12 14:00:41 +0300

    создал класс платформы

--- src/com/company/Platform.java
+++ src/com/company/Platform.java
@@ -11,5 +11,14 @@ public class Platform {
     private static int x;
     private static int y;
     private ArrayList<Image> imagesPlatforms = new ArrayList();
+    Sprite platform = new Sprite("platform1.png");
+
+    public  Platform(){
+
+    }
+
+    public void drawImage(Graphics g,int x,int y){
+        g.drawImage(platform.getImage(),x,y,null);
+    }
 
 }
