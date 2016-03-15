9acdcfe609ce2a84af111873fc0b1c61b4999339
mr-pingo <iliya936@yandex.ru>
2016-03-15 22:33:47 +0300

    физика зайчика(продолжение 1)

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,5 +1,4 @@
 package com.company;
-
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -13,13 +12,15 @@ public class Player {
     private int a;//
     private int b;// переменные для хранения номера картинки, для анимации
     private int c;//
-    private static int START_Y_POSITION=403;
     private KeyHandler keyHandler = new KeyHandler();
     private int x=0;
-    private int y=START_Y_POSITION;
+    private int y=403;
+    private boolean playerGravity=false;
     private int jump =0; // изменение координаты y при прыжке
-    private float q=0; //переменная изменения cos
+    private float alpfa=0; //переменная изменения cos
+    private float beta=1.56f; //переменная изменения cos
     private Platform platform = new Platform();
+    private boolean isCollision = true;
     ArrayList<Platform> arrayPlatforms = platform.getPlatforms();
 
     public int getX() {
@@ -64,68 +65,87 @@ public class Player {
 
     int index=0;
     public void drawImage (Graphics g, int x, int y) {  // имитируем анимацию
-        if(index==1)
-            g.drawImage(animation[a], x , y, null);
-        if(index==2)
-            g.drawImage(animation[b], x , y, null);
-        if(index==3)
-          g.drawImage(animation[c], x, y, null);
-        if(index==4){
-            g.drawImage(animation[b], x , y, null);
-            index=0;
+        if (index == 1)
+            g.drawImage(animation[a], x, y, null);
+        if (index == 2)
+            g.drawImage(animation[b], x, y, null);
+        if (index == 3)
+            g.drawImage(animation[c], x, y, null);
+        if (index == 4) {
+            g.drawImage(animation[b], x, y, null);
+            index = 0;
         }
         index++;
     }
 
-    public void render(Graphics g){
-        drawImage(g,x,y=(START_Y_POSITION- jump));
+    public void gravity(){
+        if(y<403) {
+            float b = (float) Math.cos(beta) * 10;
+            if (beta > 0) {
+                beta -= 0.11;
+                y += (int)b;
+            }
+            else
+                y += (int)b;
+            System.out.println(y);
+        }
+        else
+            beta=1.65f;
     }
 
-    public void update(){
-        if((keyHandler.isLeftpressent() == false)){
+    public void render(Graphics g){
+        drawImage(g,x,y);
+    }
+
+    public void update() {
+        gravity();
+        if ((keyHandler.isLeftpressent() == false)) {
             whoImage(0);
         }
-        if((keyHandler.isRightpressent()== false)){
+        if ((keyHandler.isRightpressent() == false)) {
             whoImage(0);
         }
-        if((keyHandler.isLeftpressent() == true)&&(x>=2)) {
+        if ((keyHandler.isLeftpressent() == true) && (x >= 2)) {
             whoImage(4, 7, 5);
-            x-=2;
+            x -= 2;
         }
-        if(keyHandler.isRightpressent() == true&&(x<=754)) {
+        if (keyHandler.isRightpressent() == true && (x <= 754)) {
             whoImage(1, 6, 2);
-            x+=2;
+            x += 2;
         }
-        if(keyHandler.isSpacepressent()==true){
-            if (q<1.57) {
-                float b = (float) Math.sin(q) * 100;
-                q += 0.11;
-                jump = (int) b;
+        if (keyHandler.isSpacepressent() == true) {
+            if (alpfa <1.56) {
+                playerGravity=true;
+                float b = (float) Math.cos(alpfa) * 10;
+                alpfa += 0.11;
+                y-=(int) b;
             }
             else {
-                q = 0;
-                //jump =0;
-                System.out.println(jump);
+                playerGravity=false;
                 keyHandler.setSpacepressent(false);
+                alpfa =0;
             }
         }
         Iterator<Platform> i = arrayPlatforms.iterator();
         while(i.hasNext()) {
             Platform timePlatform = i.next();
-            if (((getX()+getWidth()) >=(timePlatform.getX()) && (x <= (timePlatform.getX()+timePlatform.getWIDTH())) && ((getY()+getHeight()) >= timePlatform.getY()))) {
-                START_Y_POSITION = timePlatform.getY() - 74;
-                keyHandler.setSpacepressent(false);
+            if (((getX()+getWidth()) >(timePlatform.getX())) && (x < (timePlatform.getX()+timePlatform.getWIDTH())) && ((getY()+getHeight()) >= timePlatform.getY())&&((getY()+getHeight())<=(timePlatform.getY()+timePlatform.getHEIGHT()))) {
+                //условие что зайчик стоит на платформе
             }
-            if (((getX()+getWidth()) >= (timePlatform.getX())) && (x <=(timePlatform.getX()+timePlatform.getWIDTH())) && (y <= (timePlatform.getY()+timePlatform.getHEIGHT()))) {
-                keyHandler.setSpacepressent(false);
+            if (((getX()+getWidth()) >= (timePlatform.getX())) && (x <=(timePlatform.getX()+timePlatform.getWIDTH())) && (getY() == (timePlatform.getY()+timePlatform.getHEIGHT()))) {
+                //Условие что зайчик остановится ударившись головой об платформу
+               //keyHandler.setSpacepressent(false);
             }
-            if ((y <= (timePlatform.getY()+timePlatform.getHEIGHT())) && ((getX()+getWidth()) >= (timePlatform.getX()))) {
-                x = timePlatform.getX()-getWidth();
+             if ((getY() <= (timePlatform.getY()+timePlatform.getHEIGHT())) && ((getX()+getWidth()) == (timePlatform.getX()))&&((getY()+getHeight())>=timePlatform.getY())) {
+                    if(keyHandler.isRightpressent())
+                    x-=2;
+                //условие что зайчик отсановится слева
             }
-            if ((y <= (timePlatform.getY()+timePlatform.getHEIGHT())) && (x <= (timePlatform.getX()+timePlatform.getWIDTH()))) {
-                x = (timePlatform.getX()+timePlatform.getWIDTH());
+            if ((getY() <= (timePlatform.getY()+timePlatform.getHEIGHT())) && (x == (timePlatform.getX()+timePlatform.getWIDTH()))&&((getY()+getHeight())>=timePlatform.getY())){
+                //условие что зайчик отсановится справа
+                if(keyHandler.isLeftpressent())
+                    x+=2;
             }
         }
     }
-
 }
